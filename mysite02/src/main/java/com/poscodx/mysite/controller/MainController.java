7b0fc77803ae4c81a28d66c30614d727cec29818
thddb <thddb@YUNA-GB3>
2023-09-08 11:43:27 +0900

    Action/ActionFactory/WebUtil

--- mysite02/src/main/java/com/poscodx/mysite/controller/MainController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -6,14 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.poscodx.mysite.web.mvb.main.MainActionFactroy;
+import com.poscodx.web.mvc.Action;
+
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request
-			.getRequestDispatcher("WEB-INF/views/main/index.jsp")
-			.forward(request, response);
+		request.setCharacterEncoding("utf-8");
+		String actionName = request.getParameter("a");
+		Action action = new MainActionFactroy().getAction(actionName);
+
+		action.execute(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
---
+++ mysite02/src/main/java/com/poscodx/mysite/dao/UserDao.java
@@ -0,0 +1,60 @@
+package com.poscodx.mysite.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.poscodx.mysite.vo.UserVo;
+
+public class UserDao {
+	// DB Connection
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.186:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	// insert
+	public boolean insert(UserVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+
+		try {
+			conn = getConnection();
+
+			String sql = "insert into user values(null, ?, ?, password(?), ?, current_date())";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+		} catch (SQLException e) {
+			System.out.println("SQLException : " + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("SQLException : " + e);
+			}
+		}
+
+		return result;
+	}
+}
