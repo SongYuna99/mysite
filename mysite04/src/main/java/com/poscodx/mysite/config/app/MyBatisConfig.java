885de994716d46d6f791b552a7178a6aef54d198
SongYuna99 <thddbsk7248@gmail.com>
2023-10-05 14:53:12 +0900

    mysite04 : app config

---
+++ mysite04/src/main/java/com/poscodx/mysite/config/app/MyBatisConfig.java
@@ -0,0 +1,11 @@
+package com.poscodx.mysite.config.app;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+	
+	// MyBatis SqlSessionFactory (applicationContext.xml)
+
+	// MyBatis SqlSessionTemplate (applicationContext.xml)
+}
