e8ed36f460dd55444d163b00c88a8697d1de8168
SongYuna99 <thddbsk7248@gmail.com>
2023-10-06 10:51:43 +0900

    mysite04 : web config #2

--- mysite04/src/main/java/com/poscodx/mysite/config/web/FileUploadConfig.java
+++ mysite04/src/main/java/com/poscodx/mysite/config/web/FileUploadConfig.java
@@ -1,5 +1,34 @@
 package com.poscodx.mysite.config.web;
 
-public class FileUploadConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class FileUploadConfig implements WebMvcConfigurer {
+
+	// Mulitpart Resolver (spring-servlet.xml)
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(52428800);
+		multipartResolver.setMaxInMemorySize(52428800);
+		multipartResolver.setDefaultEncoding("utf-8");
+
+		return multipartResolver;
+	}
+
+	// url-resource Mapping (spring-servlet.xml)
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/assets/upload-images/**")
+			.addResourceLocations("file:/mysite-uploads/");
+	}
 
 }
--- mysite04/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite04/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -72,5 +72,15 @@ public class UserController {
 		authUser.setName(userVo.getName());
 		return "redirect:/user/update";
 	}
+	
+	@RequestMapping("/auth")
+	public void auth() {
+		
+	}
+	
+	@RequestMapping("/logout")
+	public void logout() {
+		
+	}
 
 }
