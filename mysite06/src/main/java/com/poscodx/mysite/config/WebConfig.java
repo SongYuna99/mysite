62daad46cfdd54d602e64aeb7e37156752dcd29c
SongYuna99 <thddbsk7248@gmail.com>
2023-10-11 10:56:05 +0900

    application.yml

--- mysite06/src/main/java/com/poscodx/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/poscodx/mysite/config/WebConfig.java
@@ -1,46 +1,74 @@
 package com.poscodx.mysite.config;
 
+import java.util.List;
+
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.poscodx.mysite.config.web.FileUploadConfig;
-import com.poscodx.mysite.config.web.MessageSourceConfig;
-import com.poscodx.mysite.config.web.MvcConfig;
-import com.poscodx.mysite.config.web.SecurityConfig;
-import com.poscodx.mysite.event.ApplicationContextEventListener;
+import com.poscodx.mysite.security.AuthInterceptor;
+import com.poscodx.mysite.security.AuthUserHandlerMethodArgumentResolver;
+import com.poscodx.mysite.security.LoginInterceptor;
+import com.poscodx.mysite.security.LogoutInterceptor;
 import com.poscodx.mysite.security.SiteInterceptor;
 
-@Configuration
-@EnableAspectJAutoProxy
-// context:annotation-config (spring-servlet.xml)
-@ComponentScan({ "com.poscodx.mysite.controller", "com.poscodx.mysite.exception" })
-@Import({ MvcConfig.class, SecurityConfig.class, FileUploadConfig.class, MessageSourceConfig.class })
+@SpringBootConfiguration
 public class WebConfig implements WebMvcConfigurer {
 
-	// Site Interceptor (spring-servlet.xml)
+	// Argument Resolvers (spring-servlet.xml)
+	@Bean
+	public HandlerMethodArgumentResolver handlerMethodArgumentResolver() {
+		return new AuthUserHandlerMethodArgumentResolver();
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(handlerMethodArgumentResolver());
+	}
+
+	// Interceptors (spring-servlet.xml)
+	@Bean
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+
+	@Bean
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor authInterceptor() {
+		return new AuthInterceptor();
+	}
+	
 	@Bean
 	public HandlerInterceptor siteInterceptor() {
 		return new SiteInterceptor();
 	}
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry
-		.addInterceptor(siteInterceptor())
-		.addPathPatterns("/**")
-		.excludePathPatterns("/assests/**");
+			.addInterceptor(loginInterceptor())
+			.addPathPatterns("/user/auth");
+		
+		registry
+			.addInterceptor(logoutInterceptor())
+			.addPathPatterns("/user/logout");
+		
+		registry
+			.addInterceptor(authInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/assests/**", "/user/auth", "/user/logout");
+	
+		registry
+			.addInterceptor(siteInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/assests/**");
 	}
 
-
-
-	// Application Context Event Listener (spring-servlet.xml)
-	@Bean
-	public ApplicationContextEventListener applicationContextEventListener() {
-		return new ApplicationContextEventListener();
-	}
 }
